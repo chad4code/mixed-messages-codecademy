0b644b0acb41c57275a4f28d84dd50e45d88cf1a
chadmj <106312278+chad4code@users.noreply.github.com>
2022-10-03 12:43:10 +0300

    Initialize data pool

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+// data pool
+let num = '';
+let name = '';
+let numsQuestions = [`There are ${num} books in the Old Testament.`, `Adam was ${num} years old when Seth was born.`];
+let nums = [39, 130];
+let strQuestions = [];
+let names = [];
 
 // output
 console.log('Bible Trivia Quiz: true or false?');
