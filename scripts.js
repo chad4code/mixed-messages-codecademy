70500d1b6829ad0a2e4b57c447b441f8fdd3097f
chadmj <106312278+chad4code@users.noreply.github.com>
2022-10-03 16:59:19 +0300

    Add a number question

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 // data pool
 let num = '';
 let name = '';
-let numsQuestions = [`There are ${num} books in the Old Testament.`, `Adam was ${num} years old when Seth was born.`];
-let nums = [39, 130];
+let numsQuestions = [`There are ${num} books in the Old Testament.`, `Adam was ${num} years old when Seth was born.`, `Psalm ${num} is know for the psalmist asking where he can flee from the spirit of the Lord.`];
+let nums = [39, 130, 139];
 let strQuestions = [];
 let names = [];
 
