c03478b67be40c9145d9519a78a517d09a29a99d
chadmj <106312278+chad4code@users.noreply.github.com>
2022-10-04 08:05:31 +0300

    Initial data pool complete

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 // data pool
-let num = '';
-let name = '';
-let numsQuestions = [`There are ${num} books in the Old Testament.`, `Adam was ${num} years old when Seth was born.`, `Psalm ${num} is know for the psalmist asking where he can flee from the spirit of the Lord.`];
-let nums = [39, 130, 139];
-let strQuestions = [];
-let names = [];
+let num;
+let name;
+let numsQuestions = [`There are ${num} books in the Old Testament.`, `Adam was ${num} years old when Seth was born.`, `Psalm ${num} is know for the psalmist asking where he can flee from the spirit of the Lord.`, `David was ${num} years old when he died.`, `Psalm ${num} is the shortest chapter in the Bible.`, `Zacharias the priest was ${num} years old when an angel appeared to him.`];
+let nums = [39, 130, 139, 70, 117, 83];
+let strQuestions = [`${name} was deceived by his son.`, `${name} had a wife that died before he did.`, `${name} was born in the mountains.`, `${name} was extremely rich.`, `${name} sailed the Mediterranean.`, `${name} had things in common with a rock.`];
+let names = ["Isaac", "Abraham", "Solomon", "David", "Paul", "Peter"];
 
 // output
-console.log('Bible Trivia Quiz: true or false?');
+console.log("Bible Trivia Quiz: true, false, or we don't know?");
